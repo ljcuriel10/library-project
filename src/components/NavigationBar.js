3f2616a93540c8d1d6f12b50a8b898c80ca79fc7
Luis Javier Curiel <ljcuriel0729@gmail.com>
2022-04-02 12:12:17 -0700

    Added react-icons

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,30 +1,20 @@
 import React from 'react';
 import {Nav, Navbar} from 'react-bootstrap'
-import styled from 'styled-components'
+import { IconContext } from 'react-icons';
+import {FaHome} from 'react-icons/fa'
 
-const Styles = styled.div`
-    .navbar {
-        background-color: black;
-    }
 
-    .navbar-brand, .nav-link {
-        color: white;
-
-        &:hover {
-            color: white;
-        }
-    }
-`
 
 const NavigationBar = () => (
-    <Styles>
+    <IconContext.Provider value={{style: {color:'orange'}}}>
         <Navbar bg='dark' variant='dark' expand="lg">
             <Navbar.Brand href="/">Luis's Bookstore</Navbar.Brand>
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
             <Navbar.Collapse className='justify-content-end'>
                 <Nav className='ml-auto'>
                     <Nav.Item>
-                        <Nav.Link href='/'>Home</Nav.Link>
+                       
+                        <Nav.Link href='/'><FaHome/> Home </Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
                         <Nav.Link href='/about'>About</Nav.Link>
@@ -35,7 +25,7 @@ const NavigationBar = () => (
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
-    </Styles>
+    </IconContext.Provider>
   )
 
 
